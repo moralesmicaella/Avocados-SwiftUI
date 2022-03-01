577c35060cf717a29c3f05191fa7f3380823e3c6
Micaella Morales <morales.micaella@gmail.com>
2022-03-01 01:48:53 -0800

    Initial Commit

---
+++ Avocados/App/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Micaella Morales on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
